cf221cd2c225dbf0ff736cd8a1ca03f12f004f6c
SmithAlon <160780550+SmithAlon@users.noreply.github.com>
2025-01-28 20:25:42 -0600

    Update TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
 import { TodoI } from './TodoApp';
 import TodoItem from './TodoItem';
 
 interface TodoListProps {
     todos: TodoI[];
     deleteTodo: (id: number) => void;
+    startEditing: (todo: TodoI) => void;
+    saveTodo: (updatedTodo: TodoI) => void;
+    editingTodo: TodoI | null;
     isDarkMode: boolean;
 }
 
-export default function TodoList({ todos, deleteTodo, isDarkMode }: TodoListProps) {
+export default function TodoList({ todos, deleteTodo, startEditing, saveTodo, editingTodo, isDarkMode }: TodoListProps) {
     return (
         <div>
             {todos.map((todo) => (
-                <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} isDarkMode={isDarkMode} />
+                <TodoItem 
+                    key={todo.id} 
+                    todo={todo} 
+                    deleteTodo={deleteTodo} 
+                    startEditing={startEditing} 
+                    saveTodo={saveTodo} 
+                    isEditing={editingTodo?.id === todo.id} 
+                    isDarkMode={isDarkMode} 
+                />
             ))}
         </div>
     );
